8ed544da08f1b6b92fa43a8d274037acfbd28840
Janarthanan Kannan <tjana17@gmail.com>
2026-02-14 14:50:49 +0530

    Initial Commit

---
+++ HappyValentinesDay/HappyValentinesDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyValentinesDayApp.swift
+//  HappyValentinesDay
+//
+//  Created by Janarthanan Kannan on 14/02/26.
+//
+
+import SwiftUI
+
+@main
+struct HappyValentinesDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
